39d6a6a1ca2a89c3c723d3063dae6b57299f1879
huangguangda <2397923107@qq.com>
2017-11-26 23:11:29 +0800

    1

---
+++ app/src/main/java/cn/edu/gdmec/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.android.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jack on 2017/11/26.
+ */
+
+public class BoxDrawingView extends View{
+    public BoxDrawingView(Context context){
+        this(context, null);
+    }
+    public BoxDrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+}
