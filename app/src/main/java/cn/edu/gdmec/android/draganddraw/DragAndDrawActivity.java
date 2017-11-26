d996d09cdaced97e90093282a2ab65a845fdcbe4
huangguangda <2397923107@qq.com>
2017-11-26 23:03:32 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment(){
+        return DragAndDrawFragment.newInstance();
+    }
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.fragment_drag_and_draw );
+    }*/
+}
